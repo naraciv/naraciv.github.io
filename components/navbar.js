0b0e6b79311166c0d2cf19985c66451832714010
Bulbmin <dev@bulbmin.com>
2026-04-01 02:16:59 -0400

    fixed formatting

--- components/navbar.js
+++ components/navbar.js
@@ -409,6 +409,18 @@ class CustomNavbar extends HTMLElement {
                 <a href="/heads" class="nav-dropdown-item">Head Shop</a>
               </div>
             </div>
+                        <div class="nav-dropdown">
+              <a href="/tools" class="nav-link nav-dropdown-trigger">
+                Tools
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <polyline points="6 9 12 15 18 9"></polyline>
+                </svg>
+              </a>
+              <div class="nav-dropdown-menu">
+                <a href="/snitches" class="nav-dropdown-item">Snitches</a>
+                <a href="/stats" class="nav-dropdown-item">Player Stats</a>
+              </div>
+            </div>
             <a href="/joining" class="nav-link">Join Us</a>
           </div>
           <div class="theme-switch-wrap">
